39fca23367dc49042a2de456f6e952c9dd97a849
cootcode <dawid.grabowski19@gmail.com>
2023-10-14 00:18:03 +0100

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import dsmax from "./tech/dsmax.png";
+import zbrush from "./tech/zbrush.png";
+import substance from "./tech/substance.png";
+import designer from "./tech/designer.png";
+import blender from "./tech/blender.png";
+import maya from "./tech/maya.png";
+import photoshop from "./tech/photoshop.png";
+import illustrator from "./tech/illustrator.png";
+import aftereffects from "./tech/aftereffects.png";
+import unrealengine from "./tech/unrealengine.png";
+import unity from "./tech/unity.png";
+import mudbox from "./tech/mudbox.png";
+import marvelousdesigner from "./tech/marvelousdesigner.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  dsmax,
+  zbrush,
+  substance,
+  designer,
+  blender,
+  maya,
+  photoshop,
+  illustrator,
+  aftereffects,
+  unrealengine,
+  unity,
+  mudbox,
+  marvelousdesigner,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,265 @@
+import {
+    dsmax,
+    zbrush,
+    substance,
+    designer,
+    blender,
+    maya,
+    photoshop,
+    illustrator,
+    aftereffects,
+    unrealengine,
+    unity,
+    mudbox,
+    marvelousdesigner,
+    mobile,
+    backend,
+    creator,
+    web,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "3D Artist",
+      icon: web,
+    },
+    {
+      title: "3D Animator",
+      icon: mobile,
+    },
+    {
+      title: "Texture Artist",
+      icon: backend,
+    },
+    {
+      title: "Environmental Artist",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "3Dsmax",
+      icon: dsmax,
+    },
+    {
+      name: "Zbrush",
+      icon: zbrush,
+    },
+    {
+      name: "Substance Painter",
+      icon: substance,
+    },
+    {
+      name: "Substance Designer",
+      icon: designer,
+    },
+    {
+      name: "Blender",
+      icon: blender,
+    },
+    {
+      name: "Maya",
+      icon: maya,
+    },
+    {
+      name: "Photoshop",
+      icon: photoshop,
+    },
+    {
+      name: "Illustrator",
+      icon: illustrator,
+    },
+    {
+      name: "After Effects",
+      icon: aftereffects,
+    },
+    {
+      name: "Unreal Engine",
+      icon: unrealengine,
+    },
+    {
+      name: "Unity",
+      icon: unity,
+    },
+    {
+      name: "Mudbox",
+      icon: mudbox,
+    },
+    {
+      name: "Marvelous Designer",
+      icon: marvelousdesigner,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Full-Time Student",
+      company_name: "Glasgow Caledonian University",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2021 - April 2023",
+      points: [
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores",
+        "et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est",
+        "et quas molestias excepturi sint occaecati cupiditate non provident.",
+        "et quas molestias excepturi sint occaecati cupiditate non providentumque nihil impedit quo minus id quod maxime placeat facere possimus.",
+      ],
+    },
+    {
+      title: "Full-Time Student",
+      company_name: "City Of Glasgow College",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2019 - Feb 2021",
+      points: [
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores",
+        "et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est",
+        "et quas molestias excepturi sint occaecati cupiditate non provident.",
+        "et quas molestias excepturi sint occaecati cupiditate non providentumque nihil impedit quo minus id quod maxime placeat facere possimus.",
+      ],
+    },
+    {
+      title: "Full-Time Student",
+      company_name: "Glasgow Caledonian University",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores",
+        "et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est",
+        "et quas molestias excepturi sint occaecati cupiditate non provident.",
+        "et quas molestias excepturi sint occaecati cupiditate non providentumque nihil impedit quo minus id quod maxime placeat facere possimus.",
+      ],
+    },
+    {
+      title: "Full-Time Student",
+      company_name: "City Of Glasgow College",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores",
+        "et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est",
+        "et quas molestias excepturi sint occaecati cupiditate non provident.",
+        "et quas molestias excepturi sint occaecati cupiditate non providentumque nihil impedit quo minus id quod maxime placeat facere possimus.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make an animation as beautiful as our product, but Gosia proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a texture artist who truly cares about their clients' success like Gosia does.",
+      name: "Chris Brynden",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Gosia optimized our characters RIG, our sales increased by 50%. We can't thank her enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      tags: [
+        {
+          name: "ZBrush",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Blender",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Autodesk",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      tags: [
+        {
+          name: "Substance Painter",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "UE5",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Adobe Package",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      tags: [
+        {
+          name: "Photoshop",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Mudbox",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Unity",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
